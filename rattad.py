80e47b14b8be1ed7d040d4279cb23363895e322a
kspar <kaspar.papli@gmail.com>
2017-03-31 19:17:44 +0300

    Add doc, update TODOs, assert -> assrt

--- rattad.py
+++ rattad.py
@@ -152,31 +152,6 @@ def at_least_2_unique(lst):
     return False
 
 
-### HIGH-LEVEL
-
-def must_not_have_input(node):
-    # TODO: this should not allow calls, but should allow other names
-    assert not name_in_ast(node, "input"), msg_should_have_not_used_input()
-
-
-def must_have_input(node):
-    assert name_in_ast(node, "input"), msg_should_have_used_input()
-
-
-def must_have_function_call_toplevel(node, fname):
-    assrt(ast_contains_function_call(node, fname), msg_should_have_func_call_toplevel(fname))
-
-
-def must_have_nested_function_call(outer_func_def_node, inner_func_name):
-    assert ast_contains_function_call(outer_func_def_node, inner_func_name), \
-        msg_inner_function_call_missing(outer_func_def_node.name, inner_func_name)
-
-
-def must_have_recursive_function_call(outer_func_def_node, func_name):
-    assert ast_contains_function_call(outer_func_def_node, func_name), \
-        msg_recursive_function_call_missing(func_name)
-
-
 def ast_contains_function_call(node, callee_name):
     for node in ast.walk(node):
         if isinstance(node, ast.Call) and hasattr(node, 'func') and hasattr(node.func, 'id') and node.func.id == callee_name:
@@ -184,53 +159,161 @@ def ast_contains_function_call(node, callee_name):
     return False
 
 
-def must_have_loop_while(node):
+### HIGH-LEVEL
+
+def must_not_have_input(node: ast.AST):
+    """
+    Check that subtree does NOT contain calls to function 'input'.
+    :param node:  root of subtree
+    """
+    # TODO: this should not allow calls, but should allow other Names
+    assrt(not name_in_ast(node, "input"), msg_should_have_not_used_input())
+
+
+def must_have_input(node: ast.AST):
+    """
+    Check subtree for calls to function 'input'.
+    :param node: root of subtree
+    """
+    assrt(name_in_ast(node, "input"), msg_should_have_used_input())
+
+
+def must_have_function_call(node: ast.AST, function_name: str):
+    """
+    Check subtree for nodes that represent a call to function_name. 
+    :param node: root of subtree
+    :param function_name:
+    """
+    assrt(ast_contains_function_call(node, function_name), msg_should_have_func_call_toplevel(function_name))
+
+
+def must_have_nested_function_call(outer_func_def_node: ast.FunctionDef, inner_func_name: str):
+    """
+    Check that outer_func_def_node's subtree contains a call to inner_func_name. 
+    :param outer_func_def_node: root of subtree
+    :param inner_func_name: name of function that must be called 
+    """
+    assrt(ast_contains_function_call(outer_func_def_node, inner_func_name),
+          msg_inner_function_call_missing(outer_func_def_node.name, inner_func_name))
+
+
+def must_have_recursive_function_call(outer_func_def_node: ast.FunctionDef, func_name: str):
+    """
+    Check that outer_func_def_node's subtree contains a recursive call to itself.
+    :param outer_func_def_node: root of subtree
+    :param func_name: name of the (this) recursive function
+    """
+    assrt(ast_contains_function_call(outer_func_def_node, func_name),
+          msg_recursive_function_call_missing(func_name))
+
+
+def must_have_loop_while(node: ast.AST):
+    """
+    Check subtree for nodes that represent a while-loop.
+    :param node: root of subtree 
+    """
     assrt(ast_contains(node, ast.While), msg_should_have_while())
 
 
-def must_have_loop_for(node):
+def must_have_loop_for(node: ast.AST):
+    """
+    Check subtree for nodes that represent a for-loop.
+    :param node: root of subtree
+    """
+    # TODO: consider list comprehension
     assrt(ast_contains(node, ast.For), msg_should_have_for())
 
 
-def must_have_loop(node):
+def must_have_loop(node: ast.AST):
+    """
+    Check subtree for nodes that represent any loop.
+    :param node: root of subtree 
+    """
     assrt(ast_contains(node, ast.While) or ast_contains(node, ast.For), msg_should_have_loop())
 
 
-def write_dummy_data(stdin, stdout, dummy=['1337'] * 10):
+def write_dummy_data(stdin, stdout, dummy: List[str] = ['1337'] * 10):
+    """
+    Write dummy data to stdin. This might be needed to stop the student program from blocking or for other reasons.
+    :param stdin: 
+    :param stdout: 
+    :param dummy: list of strings that are written to stdin
+    """
     for d in dummy[:-1]:
         stdin.write(d)
     stdout.reset()
     stdin.write(dummy[-1])
 
 
-def must_have_func_def_toplevel(module, fname):
+def must_have_func_def_toplevel(module_, function_name: str):
+    """
+    Check that there is a toplevel def of function_name.
+    :param module_: python-grader's m.module object
+    :param function_name:
+    """
     # TODO: add non-top-level checking as a separate function
-    assert hasattr(module, fname), msg_should_have_func_def(fname)
+    # TODO: implement using AST inspection
+    assrt(hasattr(module_, function_name), msg_should_have_func_def(function_name))
 
 
-def get_function(module, fname):
-    must_have_func_def_toplevel(module, fname)
-    return getattr(module, fname)
+def get_function(module_, function_name: str) -> function:
+    """
+    Get Python function object named function_name. Currently this function must be defined on top level.
+    :param module_: python-grader's m.module object 
+    :param function_name: 
+    :return: corresponding Python function object
+    """
+    must_have_func_def_toplevel(module_, function_name)
+    return getattr(module_, function_name)
 
 
-def get_function_def_node(node, fname):
+def get_function_def_node(node: ast.AST, function_name: str) -> ast.FunctionDef:
+    """
+    Fetch function def AST node of function_name from subtree with root node.
+    assrt()s that that there is exactly 1 definition.
+    :param node: root of subtree
+    :param function_name: 
+    :return: corresponding function def AST node
+    """
     defs = []
     for node in ast.walk(node):
-        if isinstance(node, ast.FunctionDef) and node.name == fname:
+        if isinstance(node, ast.FunctionDef) and node.name == function_name:
             defs.append(node)
-    assert len(defs) <= 1, msg_should_not_have_more_than_one_def(fname)
-    assert len(defs) >= 1, msg_should_have_func_def(fname)
+    assrt(len(defs) <= 1, msg_should_not_have_more_than_one_def(function_name))
+    assrt(len(defs) >= 1, msg_should_have_func_def(function_name))
     return defs[0]
 
 
-def must_have_n_params(func_def_node, no_of_args):
-    assert len(func_def_node.args.args) == no_of_args, \
-        msg_wrong_number_of_params(func_def_node.name, no_of_args, len(func_def_node.args.args))
+def must_have_n_params(func_def_node: ast.FunctionDef, no_of_params: int):
+    """
+    Checks that func_def_node is defined with exactly no_of_params params. 
+    :param func_def_node: AST node of function def
+    :param no_of_params: number of parameters
+    """
+    assrt(len(func_def_node.args.args) == no_of_params,
+          msg_wrong_number_of_params(func_def_node.name, no_of_params, len(func_def_node.args.args)))
 
 
-def must_have_equal_return_values(expected_func_object, actual_func_object, function_name, *func_args,
-                                  equalizer=equal_simple, ret_representer=quote, args_repr=None):
-    # TODO: add checking for global vars change
+def must_have_equal_return_values(expected_func_object: function, actual_func_object: function, function_name: str, *func_args: Any,
+                                  equalizer: function = equal_simple, ret_representer: function = quote, args_repr: str = None):
+    """
+    Check if two given Python function objects have equal return values when provided with *func_args.
+    
+    A custom equalizer can be provided for return value equality check (default is ==). The equalizer must
+    have 2 params and return a bool.
+    A custom representer can be provided for displaying the (differing) return values. The representer should
+    return a string.
+    A custom representation of given arguments can be provided as a string. Note that all arguments must be 
+    included in this representation. This can be used for argument types that don't repr() well, i.e. matrices.  
+    
+    :param expected_func_object: 
+    :param actual_func_object: 
+    :param function_name: 
+    :param func_args: 
+    :param equalizer: 
+    :param ret_representer: 
+    :param args_repr: 
+    """
     # TODO: should have a list of args instead of *
     # TODO: handle funcs with no params
     expected_return = expected_func_object(*func_args)
@@ -252,15 +335,24 @@ def must_have_equal_return_values(expected_func_object, actual_func_object, func
                                    ret_representer(actual_return))
 
 
-def must_have_pure_func(expected_func_object: Callable, actual_func_object: Callable, function_name: str,
+def must_have_pure_func(expected_func_object: function, actual_func_object: function, function_name: str,
                         real_args: List[Any], mock_args: List[Any], equalizer=equal_simple, ret_representer=quote, args_repr=None):
     """
-    Uses real and mock arguments to check if a given student function is pure (referentially transparent).
+    Use real and mock arguments to check if a given student function is pure (referentially transparent).
     The same mock args should be provided to the student program beforehand.
     The student function actual_func_object is considered to be nonpure iff
     equalizer(actual_func_object(*real_args), expected_func_object(*mock_args)).
     Otherwise the student function is considered as correct or incorrect based on its return value.
     NB! expected_func_object(*real_args) must NOT return None!
+    
+    :param expected_func_object: 
+    :param actual_func_object: 
+    :param function_name: 
+    :param real_args: 
+    :param mock_args: 
+    :param equalizer: see must_have_equal_return_values
+    :param ret_representer: see must_have_equal_return_values
+    :param args_repr: see must_have_equal_return_values
     """
     expected_return = expected_func_object(*real_args)
     actual_return = actual_func_object(*real_args)
@@ -270,7 +362,7 @@ def must_have_pure_func(expected_func_object: Callable, actual_func_object: Call
         # TODO: should this use repr or str?
         args_repr = reduce(lambda a1, a2: repr(a1) + ', ' + repr(a2), real_args)
 
-    assert expected_return is not None, 'Solution function should not return None'
+    assert expected_return is not None, 'Solution function must not return None'
 
     # Student function should not return None
     assrt(actual_return is not None, msg_should_not_have_returned_none(function_name))
@@ -283,9 +375,23 @@ def must_have_pure_func(expected_func_object: Callable, actual_func_object: Call
                                                                             ret_representer(expected_return), ret_representer(actual_return)))
 
 
-def must_have_correct_random_return_value(actual_func_object, function_name, func_args,
-                                          validation_function, uniqueness_function=at_least_2_unique, samples=100):
-    # TODO: add checking for global vars change
+def must_have_correct_random_return_value(actual_func_object: function, function_name: str, func_args: List[Any],
+                                          validation_function: function, uniqueness_function: function = at_least_2_unique, samples: int = 100):
+    """
+    Run the given function samples times. Check if each return value is valid using validation_function and
+    if the return values are 'random enough' using uniqueness_function.
+    The validation_function will be called with 1 argument - return value of the student function - and is expected to 
+    return a bool representing whether the given return value is valid.
+    A custom uniqueness_function with 1 parameter and bool return value can be provided. This function will be called
+    with a list of all return values of the student function and is expected to return a bool whether the values are
+    'unique enough' (default requires at least 2 distinct values).
+    :param actual_func_object: 
+    :param function_name: 
+    :param func_args: 
+    :param validation_function: function with 1 parameter and bool return value
+    :param uniqueness_function: 
+    :param samples: 
+    """
     return_values = []
     for _ in range(samples):
         actual_return = actual_func_object(*func_args)
@@ -295,8 +401,20 @@ def must_have_correct_random_return_value(actual_func_object, function_name, fun
     assrt(uniqueness_function(return_values), msg_no_distinct_return_values_from_random(function_name, samples))
 
 
-def must_have_correct_output_str(stdin, stdout, inputs, expected_strings, unexpected_strings,
-                                 exp_out_representer=quote, act_out_representer=quote, ignore_case=True):
+def must_have_correct_output_str(stdin, stdout, inputs: List[Any], expected_strings: List[str], unexpected_strings: List[str],
+                                 exp_out_representer: function = quote, act_out_representer: function = quote, ignore_case: bool = True):
+    """
+    Write inputs to stdin. Read stdout. Check if all strings in expected_strings are contained in out.
+    Check if none of the strings in unexpected_strings are contained in out.
+    :param stdin: 
+    :param stdout: 
+    :param inputs: 
+    :param expected_strings: 
+    :param unexpected_strings: 
+    :param exp_out_representer: 
+    :param act_out_representer: 
+    :param ignore_case: 
+    """
     # write all but the last input
     if len(inputs) > 1:
         for inp in inputs[:-1]:
@@ -326,8 +444,10 @@ def must_have_correct_output_str(stdin, stdout, inputs, expected_strings, unexpe
 def write_inputs(stdin, stdout, inputs: List[str]):
     """
     Write inputs to stdin. Reset output stream after writing.
+    :param stdin: 
+    :param stdout: 
+    :param inputs: 
     """
-
     # write all but the last input
     if len(inputs) > 1:
         for inp in inputs[:-1]:
